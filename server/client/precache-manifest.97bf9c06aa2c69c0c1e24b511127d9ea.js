2ceeb68a89cfff6f22e265c6697d40ef18424c36
ErSachinVashist <sachinvashist82@gmail.com>
2019-09-30 02:30:58 +0530

    missing folders added

---
+++ server/client/precache-manifest.97bf9c06aa2c69c0c1e24b511127d9ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e7e5cfb3a74903913b7",
+    "url": "/static/css/main.061dfead.chunk.css"
+  },
+  {
+    "revision": "1e7e5cfb3a74903913b7",
+    "url": "/static/js/main.1e7e5cfb.chunk.js"
+  },
+  {
+    "revision": "90de9628b4fd60a6a800",
+    "url": "/static/js/1.90de9628.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11a89a8d43302df7df330db77f324caa",
+    "url": "/static/media/user.11a89a8d.png"
+  },
+  {
+    "revision": "ba6195597411edf207c06b76fa88ba5b",
+    "url": "/index.html"
+  }
+];
